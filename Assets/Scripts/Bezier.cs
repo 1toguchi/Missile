a8ca56acc8b45469a1e364c3621489f19c118123
yuu <ytksy1218@gmail.com>
2018-12-17 20:56:22 +0900

    Add Bezier

---
+++ Assets/Scripts/Bezier.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+	/// <summary>
+	/// ベジェ曲線を描く
+	/// </summary>
+	public class BezierCurve
+	{
+		public Vector3[] ControlPoints { get; set; }
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public BezierCurve(Vector3[] controlPoints)
+		{
+			ControlPoints = controlPoints;
+		}
+
+		/// <summary>
+		/// ベジェ曲線関数
+		/// </summary>
+		public Vector3 Evaluate(float t)
+		{
+			if (ControlPoints == null)
+			{
+				return Vector3.zero;
+			}
+
+			Vector3 result = Vector3.zero;
+			int n = ControlPoints.Length;
+			for (int i = 0; i < n; i++)
+			{
+				result += ControlPoints[i] * Bernstein(n - 1, i, t);
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// バーンスタイン基底関数
+		/// </summary>
+		static float Bernstein(int n, int i, float t)
+		{
+			return Binomial(n, i) * Mathf.Pow(t, i) * Mathf.Pow(1 - t, n - i);
+		}
+
+		/// <summary>
+		/// 二項係数を計算する
+		/// </summary>
+		static float Binomial(int n, int k)
+		{
+			return Factorial(n) / (Factorial(k) * Factorial(n - k));
+		}
+
+		/// <summary>
+		/// 階乗を計算する
+		/// </summary>
+		static float Factorial(int a)
+		{
+			float result = 1f;
+			for (int i = 2; i <= a; i++)
+			{
+				result *= i;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Assets/Scripts/BezierController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Utility;
+
+public class BezierController : MonoBehaviour
+{
+	private Vector3 _endPoint;
+	private Vector3 _startPoint;
+	private Vector3 _controlPoint1;
+	private Vector3 _controlPoint2;
+	private float _t = 0.0f;
+	private BezierCurve _b;
+	private Vector3[] _controlPoints;
+	float _time = 0.0f;
+ 	
+	// Use this for initialization
+	void Start ()
+	{
+		_startPoint = transform.position;
+		_endPoint = GameObject.Find("EndPoint").gameObject.transform.position;
+		_controlPoint1 = GameObject.Find("ControlPoint_1").gameObject.transform.position;
+		_controlPoint2 = GameObject.Find("ControlPoint_2").gameObject.transform.position;
+
+		_controlPoints = new[] {_startPoint,_controlPoint1, _controlPoint2, _endPoint};
+		_b = new BezierCurve(_controlPoints);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_time += Time.deltaTime;
+
+		if (_time >= 0.01f)
+		{
+			_t += 0.01f;
+			_time = 0.0f;
+		}
+
+		if (_t >= 1.0f)
+		{
+			_t = 0.0f;
+		}
+		
+		transform.position = _b.Evaluate(_t);
+	}
+}
